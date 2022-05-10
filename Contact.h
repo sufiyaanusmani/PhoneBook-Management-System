032db3df8f901dbf208647830ed3d68b4700daf8
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-05-10 22:09:02 +0500

    initial commit

---
+++ Contact.h
@@ -0,0 +1,22 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include <conio.h>
+using namespace std;
+
+class Contact
+{
+	private:
+		char name[30];
+		char contactNumber[11];
+		char email[30];
+	public:
+		
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "Contact.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int choice;
+	Contact contact
+	while(1){
+		system("cls");
+		cout << "1. Add new contact" << endl;
+		cout << "2. View all contacts" << endl;
+		cout << "3. Search contact" << endl;
+		cout << "4. Update Contact" << endl;
+		cout << "5. Delete Contact" << endl;
+		cout << endl << "Enter your choice: ";
+		fflush(stdin);
+		cin >> choice;
+		switch(choice){
+			
+		}
+	}
+	return 0;
+}
